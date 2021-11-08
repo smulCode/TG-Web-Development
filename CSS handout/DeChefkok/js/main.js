a2973a8156c5e97df66bd70878e0ebe5e9667242
smulCode <67417862+smulCode@users.noreply.github.com>
2021-11-08 17:34:16 +0100

    V1.2 DeChefkok

---
+++ CSS handout/DeChefkok/js/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+//Hamburger-menu-nav
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+//navbar scroll events
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".main");
+
+
+const sectionOneOptions = {
+  rootMargin: "-600px 0px 0px 0px",
+};
+
+const sectionOneObservers = new IntersectionObserver(function (
+  entries,
+  sectionOneObservers
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObservers.observe(sectionOne);
+
+
+
+///overlay
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
